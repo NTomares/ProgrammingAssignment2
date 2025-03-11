e8601aa724a36ecc120c3ffd763688523f662936
Noah Tomares <ntomares@newfrontierdata.com>
2025-03-11 15:59:46 -0400

    Attempt to update .R file with required functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -14,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
   getinverse <- function() inv
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-## Write a short comment describing this function
+
 
   cacheSolve <- function(x, ...) {
     inv <- x$getinverse()
